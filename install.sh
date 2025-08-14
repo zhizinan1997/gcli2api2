d5555670a94c0c3560ead692c1793830b31a9c3f
sukaka <3493227712@qq.com>
2025-08-14 21:44:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 apt install python git
 git clone https://github.com/su-kaka/gcli2api.git
 cd ./gcli2api
-pip install -r requirements.txt
+pip install -r requirements.txt --break-system-packages
 python web.py
