abe30b7b039018e9e852a4f56f482bad3ae72129
su-kaka <101160581+su-kaka@users.noreply.github.com>
2025-08-15 00:57:43 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-pkg install python git ninja clang cmake
+pkg install rust build-essential python git ninja clang cmake
 git clone https://github.com/su-kaka/gcli2api.git
 cd ./gcli2api
 pip install -r requirements.txt --break-system-packages
