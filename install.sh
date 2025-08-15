41146aefa91eaf5da37d11c9b609345a46678eb8
sukaka <3493227712@qq.com>
2025-08-15 20:05:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-apt install git uv
+apt install git uv pm2
 git clone https://github.com/su-kaka/gcli2api.git
 cd ./gcli2api
 uv sync
-source ./venv/Scripts/activate
-python web.py
+source .venv/Scripts/activate
+pm2 start python web.py
