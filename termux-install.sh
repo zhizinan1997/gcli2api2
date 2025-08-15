66865353a5aec87947d775c2d8479c9ed6ca5b25
sukaka <3493227712@qq.com>
2025-08-15 23:19:04 +0800

    Update termux-install.sh

--- termux-install.sh
+++ termux-install.sh
@@ -1,5 +1,5 @@
 pkg update && pkg upgrade -y
-pkg install rust build-essential python git ninja clang cmake uv nodejs -y
+pkg install rust build-essential python git ninja clang cmake uv nodejs binutils-is-llvm -y
 npm install pm2 -g
 git clone https://github.com/su-kaka/gcli2api.git
 cd ./gcli2api
