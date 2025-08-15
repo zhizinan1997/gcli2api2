f06d16e2fe41b3d47fb87f1ab03b8963e0ba31d2
sukaka <3493227712@qq.com>
2025-08-15 22:41:04 +0800

    git pull

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ curl -Ls https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env.sh
 git clone https://github.com/su-kaka/gcli2api.git
 cd ./gcli2api
+git pull
 uv sync
 source .venv/bin/activate
 python3 web.py
--- termux-install.sh
+++ termux-install.sh
@@ -3,6 +3,7 @@ pkg install rust build-essential python git ninja clang cmake uv nodejs -y
 npm install pm2 -g
 git clone https://github.com/su-kaka/gcli2api.git
 cd ./gcli2api
+git pull
 uv sync
 source .venv/bin/activate
 pm2 start python3 web.py
