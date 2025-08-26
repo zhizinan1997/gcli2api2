0cb47a3ab54c592994b4439c5a14312d367b3d5e
sukaka <3493227712@qq.com>
2025-08-26 18:06:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -196,7 +196,7 @@ def get_retry_429_max_retries() -> int:
         except ValueError:
             pass
     
-    return int(get_config_value("retry_429_max_retries", 10))
+    return int(get_config_value("retry_429_max_retries", 5))
 
 def get_retry_429_enabled() -> bool:
     """Get 429 retry enabled setting."""
@@ -215,7 +215,7 @@ def get_retry_429_interval() -> float:
         except ValueError:
             pass
     
-    return float(get_config_value("retry_429_interval", 0.5))
+    return float(get_config_value("retry_429_interval", 1))
 
 def get_log_level() -> str:
     """
