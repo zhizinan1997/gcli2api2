3481631e703434972d70e48e29d1b04a75af7c4f
su-kaka <3493227712@qq.com>
2025-09-04 22:00:23 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
 echo "强制同步项目代码，忽略本地修改..."
 git fetch --all
-git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)
+for /f "delims=" %%b in ('git rev-parse --abbrev-ref HEAD') do set branch=%%b
+git reset --hard origin/%branch%
 uv sync
 call .venv\Scripts\activate.bat
 python web.py
